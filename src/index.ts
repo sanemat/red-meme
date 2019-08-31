bde5296846410ab7515101c8fe22b8fd3d93a17e
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-08-31 22:44:08 +0700

    refactor(menu): use first class only

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-const menuVideos = document.getElementsByClassName("menu__video");
-const menuDetails = document.getElementsByClassName("menu__details");
+const menuVideo = document.getElementsByClassName("menu__video")[0];
+const menuDetails = document.getElementsByClassName("menu__details")[0];
 
 const notifyFile = (file: File): void => {
-  for (let i = 0; i < menuDetails.length; i++) {
-    menuDetails[0].textContent = `
+  menuDetails.textContent = `
     name: ${file.name};
     lastModified: ${file.lastModified};
     size: ${file.size};
     type: ${file.type};
     `;
-  }
 };
 
-for (let i = 0; i < menuVideos.length; i++) {
-  menuVideos[i].addEventListener("change", event => {
-    const file = (event.target as HTMLInputElement).files[0];
-    notifyFile(file);
-  });
-}
+menuVideo.addEventListener("change", event => {
+  const file = (event.target as HTMLInputElement).files[0];
+  notifyFile(file);
+});
