e78144ea222f7840f873e914980d601295b4a0f3
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-09-01 00:48:05 +0700

    feat(menu): start

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 const menuVideo = document.getElementsByClassName("menu__video")[0];
 const menuDetails = document.getElementsByClassName("menu__details")[0];
+const menuStart = document.getElementsByClassName(
+  "menu__start"
+)[0] as HTMLInputElement;
 
 const notifyFile = (file: File): void => {
   menuDetails.textContent = `
@@ -8,6 +11,10 @@ const notifyFile = (file: File): void => {
     size: ${file.size};
     type: ${file.type};
     `;
+};
+
+const playable = (): void => {
+  menuStart.disabled = false;
 };
 
 menuVideo.addEventListener("change", event => {
