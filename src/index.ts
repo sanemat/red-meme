a0ec3304a50e3d6ae7d2f101cffb9a86d8bff05c
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-08-31 22:16:57 +0700

    feat(menu): show video details

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 const menuVideos = document.getElementsByClassName("menu__video");
+const menuDetails = document.getElementsByClassName("menu__details");
+
+const notifyFile = (file: File): void => {
+  for (let i = 0; i < menuDetails.length; i++) {
+    menuDetails[0].textContent = `
+    name: ${file.name};
+    lastModified: ${file.lastModified};
+    size: ${file.size};
+    type: ${file.type};
+    `;
+  }
+};
+
 for (let i = 0; i < menuVideos.length; i++) {
   menuVideos[i].addEventListener("change", event => {
     const file = (event.target as HTMLInputElement).files[0];
-    console.log(file);
+    notifyFile(file);
   });
 }
